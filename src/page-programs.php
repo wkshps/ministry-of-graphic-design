11500509b0fe028e2365eb6931482f893ba54c4a
Eric Price <eric@ericprice.cc>
2018-10-04 18:42:44 -0400

    lots

--- src/page-programs.php
+++ src/page-programs.php
@@ -1,4 +1,22 @@
+<?php
+/**
+ * Template Name: Programs
+ */
+?>
 <?php get_header(); ?>
+
+<?php
+  $args = array(
+    'posts_per_page' => -1,
+    'post_type' => 'page',
+    'post_parent' => $post->ID,
+    'orderby' => 'meta_value',
+    'meta_key' => 'start_date',
+    'order' => 'ASC'
+  );
+
+  $the_query = new WP_Query($args);
+?>
 
 <?php while ( have_posts() ): the_post(); ?>
   <div class="flipper flipper-content">
@@ -25,26 +43,38 @@
       </header>
 
       <div class="content">
-        <?php if (get_field('end_date')): ?>
-          <?php
-            $start_date = new DateTime(get_field('start_date'));
-            $end_date = new DateTime(get_field('end_date'));
-          ?>
-          <?php if ($start_date->format('M') == $end_date->format('M')): ?>
-            <div class="program-date"><?php echo $start_date->format('F j'); ?>&ndash;<?php echo $end_date->format('j'); ?></div>
-          <?php else: ?>
-            <div class="program-date"><?php echo $start_date->format('F j'); ?> &ndash; <?php echo $end_date->format('F j'); ?></div>
-          <?php endif; ?>
-        <?php elseif (get_field('start_date')): ?>
-          <div class="program-date"><?php echo $start_date->format('M j'); ?></div>
-        <?php endif; ?>
-        <?php if (get_field('time')): ?>
-          <div class="program-time"><?php the_field('time'); ?></div>
-        <?php endif; ?>
         <?php if (get_the_title() != 'Home'): ?>
-          <h1 class="page-title <?php if (get_field('start_date')): ?>program-title<?php endif; ?>"><?php the_title(); ?></h1>
+          <h1 class="page-title"><?php the_title(); ?></h1>
         <?php endif; ?>
         <?php the_content(); ?>
+      </div>
+      <?php if ($the_query->have_posts()): ?>
+        <ul class="schedule">
+          <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
+            <li class="schedule-item">
+              <a href="<?php the_permalink(); ?>">
+                <?php if (get_field('end_date')): ?>
+                  <?php
+                    $start_date = new DateTime(get_field('start_date'));
+                    $end_date = new DateTime(get_field('end_date'));
+                  ?>
+                  <?php if ($start_date->format('M') == $end_date->format('M')): ?>
+                    <div class="schedule-item-date"><?php echo $start_date->format('M j'); ?>&ndash;<?php echo $end_date->format('j'); ?></div>
+                  <?php else: ?>
+                    <div class="schedule-item-date"><?php echo $start_date->format('M j'); ?> &ndash; <?php echo $end_date->format('M j'); ?></div>
+                  <?php endif; ?>
+                <?php else: ?>
+                  <div class="schedule-item-date"><?php echo $start_date->format('M j'); ?></div>
+                <?php endif; ?>
+                <?php if (get_field('time')): ?>
+                  <div class="schedule-item-time"><?php the_field('time'); ?></div>
+                <?php endif; ?>
+                <div class="schedule-item-title"><?php the_title(); ?></div>
+              </a>
+            </li>
+          <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+      <?php endif; ?>
         &nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>&nbsp;
       </div>
     </div>
